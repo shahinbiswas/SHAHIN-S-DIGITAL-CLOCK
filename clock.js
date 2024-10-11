9ea0c4ec6043caba64ac29931142a4d941549c0c
shahinbiswas <shahinulislam8397@gmail>
2024-10-12 00:07:51 +0600

    initial commit

---
+++ clock.js
@@ -0,0 +1,28 @@
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
+const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "PM";
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
